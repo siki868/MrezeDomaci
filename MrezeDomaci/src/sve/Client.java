9bb6ab9b4531031320bb5973b2c50825eb339af2
siki868 <ndimitrijevic16@raf.edu.rs>
2018-05-03 22:47:41 +0200

    Prenos fajla preko UDP-a

--- MrezeDomaci/src/sve/Client.java
+++ MrezeDomaci/src/sve/Client.java
@@ -1,5 +1,35 @@
 package sve;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Client {
+
+	private static String PUTANJA = "C:/Users/Siki/Desktop/p.txt";
+
+	public Client() throws Exception {
+
+		DatagramSocket socket = new DatagramSocket();
+		Path path = Paths.get(PUTANJA);
+		byte[] buffer = Files.readAllBytes(path);
+
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName("localhost"), 2018);
+		socket.send(packet);
+
+		socket.close();
+	}
+
+	public static void main(String[] args) {
+		try {
+			new Client();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
--- MrezeDomaci/src/sve/Server.java
+++ MrezeDomaci/src/sve/Server.java
@@ -1,5 +1,41 @@
 package sve;
 
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class Server {
+
+	private static String PUTANJA = "C:/Users/Siki/Desktop/a.txt";
+
+	public Server() throws Exception {
+
+		DatagramSocket socket = new DatagramSocket(2018);
+
+		byte[] buffer = new byte[1500];
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+		socket.receive(packet);
+
+
+		
+		String sve = new String(buffer).trim();
+		
+		PrintWriter pw = new PrintWriter(PUTANJA);
+		pw.print(sve);
+		pw.close();
+		socket.close();
+
+	}
+
+
+	public static void main(String[] args) {
+		try {
+			new Server();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
