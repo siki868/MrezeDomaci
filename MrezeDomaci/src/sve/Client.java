b17512edbfce1cce0a9994c5a6da1332b2aa8262
Siki <Siki@DESKTOP-3DQHCT3>
2018-05-05 20:56:38 +0200

    Zavrseno sve, malo guija malo konzole

--- MrezeDomaci/src/sve/Client.java
+++ MrezeDomaci/src/sve/Client.java
@@ -1,27 +1,151 @@
 package sve;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class Client {
-
-	private static String PUTANJA = "C:/Users/Siki/Desktop/p.txt";
+	//C:/Users/Siki/Desktop/download.txt
+	private static String PUTANJA = JOptionPane.showInputDialog("Unesite path") + "/download.txt";
 
 	public Client() throws Exception {
 
+		Scanner sc = new Scanner(System.in);
+		
+		
 		DatagramSocket socket = new DatagramSocket();
-		Path path = Paths.get(PUTANJA);
-		byte[] buffer = Files.readAllBytes(path);
+		InetAddress ip = InetAddress.getByName("localhost");
+		FileWriter fw = new FileWriter(PUTANJA, true);
+		
+		
+		while(true) {
+			System.out.println("Download Upload Read Quit");
+			
+			String msg = sc.nextLine();
+			byte[] buffer = msg.getBytes();
 
-		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName("localhost"), 2018);
-		socket.send(packet);
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length, ip, 2018);
+			socket.send(packet);
+			
+			if(msg.equalsIgnoreCase("download")) {
+				buffer = new byte[300];
+				packet = new DatagramPacket(buffer, buffer.length);
+				socket.receive(packet);
+				String a = new String(buffer).trim();
+				System.out.println(a);
+				
+				
+				String odabir = sc.nextLine();
+				System.out.println("Izabrali ste " + odabir);
+				packet = new DatagramPacket(odabir.getBytes(), odabir.getBytes().length, ip, 2018);
+				socket.send(packet);
+				
+				//primanje file-a
+				while(true) {
+					buffer = new byte[512];
+					packet = new DatagramPacket(buffer, buffer.length);
+					socket.receive(packet);
+					String por = new String(buffer).trim();
+					if(por.equals("KRAJ")) break;
+					fw.write(por);
+					String dobro = "Primio";
+					packet = new DatagramPacket(dobro.getBytes(), dobro.getBytes().length, ip, 2018);
+					socket.send(packet);
+				}
+				fw.close();
+			}else if(msg.equalsIgnoreCase("upload")) {
+				String noviFile = JOptionPane.showInputDialog("Path fajla za upload");
+				File f = new File(noviFile);
+				if(f.exists()) {
+					buffer = noviFile.getBytes();
+					packet = new DatagramPacket(buffer, buffer.length, ip, 2018);
+					socket.send(packet);
+					System.out.println("Poslao uspesno!");
+				}
+			}else if(msg.equalsIgnoreCase("read")) {
+				
+				
+				
+				buffer = new byte[300];
+				packet = new DatagramPacket(buffer, buffer.length);
+				socket.receive(packet);
+				String a = new String(buffer).trim();
+				System.out.println(a);
+				
+				String odabir = sc.nextLine();
+				System.out.println("Izabrali ste " + odabir);
+				packet = new DatagramPacket(odabir.getBytes(), odabir.getBytes().length, ip, 2018);
+				socket.send(packet);
+				
+				
+				JFrame frame = new JFrame("Read");
+				JTextArea area = new JTextArea();
+				JScrollPane pane = new JScrollPane(area);
+				pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+				frame.setVisible(true);
+				frame.setLocationRelativeTo(null);
+				frame.setSize(300, 240);
+				frame.add(pane);
+				
+				while(true) {
+					buffer = new byte[512];
+					packet = new DatagramPacket(buffer, buffer.length);
+					socket.receive(packet);
+					String por = new String(buffer).trim();
+					if(por.equals("KRAJ")) break;
+					area.append(por);
+					String dobro = "Primio";
+					packet = new DatagramPacket(dobro.getBytes(), dobro.getBytes().length, ip, 2018);
+					socket.send(packet);
+				}
+			}else if(msg.equalsIgnoreCase("quit")) {
+				String q = "QUIT";
+				buffer = q.getBytes();
+				packet = new DatagramPacket(buffer, buffer.length, ip, 2018);
+				socket.send(packet);
+				socket.close();
+				sc.close();
+				break;
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 		socket.close();
 	}
+	
+	
+	
+	public boolean isEmptyArray(byte [] array){
+		for (byte b : array) {
+		    if (b != 0) {
+		        return false;
+		    }
+		}
+		return true;
+	}
+	
+	
+	
+	
+	
 
 	public static void main(String[] args) {
 		try {
--- MrezeDomaci/src/sve/Server.java
+++ MrezeDomaci/src/sve/Server.java
@@ -1,32 +1,125 @@
 package sve;
 
-import java.io.PrintWriter;
+import java.io.File;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Server {
 
-	private static String PUTANJA = "C:/Users/Siki/Desktop/a.txt";
+	
+	private ArrayList<String> fajlovi = new ArrayList<>();
 
 	public Server() throws Exception {
 
+		fajlovi.add("C:/Users/Siki/Desktop/provera/a.txt");
+		fajlovi.add("C:/Users/Siki/Desktop/provera/1.txt");
+		fajlovi.add("C:/Users/Siki/Desktop/provera/2.txt");
+		fajlovi.add("C:/Users/Siki/Desktop/provera/3.txt");
+		fajlovi.add("C:/Users/Siki/Desktop/provera/4.txt");
+		fajlovi.add("C:/Users/Siki/Desktop/provera/5.txt");
+		//fajlovi.add("C:/Users/Siki/Desktop/provera/6.txt");
+		
+		
+		
+		
 		DatagramSocket socket = new DatagramSocket(2018);
-
-		byte[] buffer = new byte[1500];
-		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
-		socket.receive(packet);
-
-
 		
-		String sve = new String(buffer).trim();
 		
-		PrintWriter pw = new PrintWriter(PUTANJA);
-		pw.print(sve);
-		pw.close();
-		socket.close();
-
+		
+		while(true) {
+			
+			//prima opciju
+			byte[] buffer = new byte[500];
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			socket.receive(packet);
+			InetAddress sender = packet.getAddress();
+			int portSendera = packet.getPort();
+			String op = new String(buffer).trim();
+			
+			System.out.println("Klijent " + sender + " je izabrao " + op);
+			
+			
+			//skidanje
+			if(op.equalsIgnoreCase("download") || op.equalsIgnoreCase("read")) {
+				//salje opcije svih fajlova koji mogu da se skinu
+				String opcije = "";
+				for(String f: fajlovi) {
+					File file = new File(f);
+					opcije += file.getName() + "\n";
+				}
+				buffer = opcije.getBytes();
+				packet = new DatagramPacket(buffer, buffer.length, sender, portSendera);
+				socket.send(packet);
+				
+				//prima odabir od klijenta
+				buffer = new byte[100];
+				packet = new DatagramPacket(buffer, buffer.length);
+				socket.receive(packet);
+				String odabir = new String(buffer).trim();
+				for(String s: fajlovi) {
+					if(s.contains(odabir)) {
+						odabir = s;
+					}
+				}
+				//slanje file-a
+				Path path = Paths.get(odabir);
+				byte[] data = Files.readAllBytes(path);
+				byte[] chunk = new byte[512];
+				if(data.length < 512) {
+					packet = new DatagramPacket(data, data.length, sender, portSendera);
+					socket.send(packet);
+				}else {
+					int j = 0;
+					for(int i = 0; i < data.length-1; i++) {
+						if(i % 511 == 0 && i != 0) {
+							packet = new DatagramPacket(chunk, chunk.length, sender, portSendera);
+							socket.send(packet);
+							
+							packet = new DatagramPacket(buffer, buffer.length);
+							socket.receive(packet);
+							String odg = new String(buffer).trim();
+							if(!odg.equals("Primio")) {
+								 i-=512;
+								 if(i < 0) i = 0;
+								 continue;
+							}
+							
+							chunk = new byte[512];
+							j = 0;
+						}
+						chunk[j++] = data[i];
+					}
+				}
+				String kraj = "KRAJ";
+				buffer = kraj.getBytes();
+				packet = new DatagramPacket(buffer, buffer.length, sender, portSendera);
+				socket.send(packet);
+			}else if(op.equalsIgnoreCase("upload")) {
+				buffer = new byte[100];
+				packet = new DatagramPacket(buffer, buffer.length);
+				socket.receive(packet);
+				String noviPath = new String(buffer).trim();
+				System.out.println("Dodat novi file: " + noviPath);
+				fajlovi.add(noviPath);
+			}else if(op.equalsIgnoreCase("QUIT")) {
+				socket.close();
+				break;
+			}
+		}
+		
+		
+		
+		
+		
 	}
+		
+
+
 
 
 	public static void main(String[] args) {
